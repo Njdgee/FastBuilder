9925379ea33769644b298fb7becb2f6315ac65d3
Njdge <83909076+Njdgee@users.noreply.github.com>
2024-07-25 14:55:54 +0800

    [*] MongoDB Update && Config.yml

--- src/main/java/com/njdge/fastbuilder/database/MongoManager.java
+++ src/main/java/com/njdge/fastbuilder/database/MongoManager.java
@@ -21,13 +21,13 @@ public class MongoManager {
     public MongoManager(FastBuilder plugin) {
         this.plugin = plugin;
         this.init();
-        this.enabled = plugin.getConfig().getBoolean("mongo.enabled");
+        this.enabled = plugin.getConfig().getConfiguration().getBoolean("mongo.enabled");
     }
 
     public void init() {
-        String uri = plugin.getConfig().getString("mongo.uri");
+        String uri = plugin.getConfig().getConfiguration().getString("mongo.uri");
         this.client = MongoClients.create(uri);
-        this.database = client.getDatabase(plugin.getConfig().getString("mongo.database"));
+        this.database = client.getDatabase(plugin.getConfig().getConfiguration().getString("mongo.database"));
         this.loadCollections();
     }
 
